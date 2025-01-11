c9e9bf135fc187992bf807672b676dc1156dcd83
phantom-clipz <it21831768@my.sliit.lk>
2025-01-12 01:47:45 +0530

    app changes with layout and index creation

---
+++ components/Providers/index.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+import store from '@/hooks/store';
+import { Provider } from 'react-redux';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+   return (
+      <>
+         <Provider store={store}>
+            <main>{children}</main>
+         </Provider>
+      </>
+   );
+};
+
+export default Providers;
