7bf06cf00c62741d48b5237fa8c10fbffa5c255e
phantom-clipz <it21831768@my.sliit.lk>
2025-01-13 00:21:51 +0530

    counter update with pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Counter } from '@/components/Common/Counter/Counter';
 
 export default function Home() {
--- components/Common/Counter/Counter.tsx
+++ components/Common/Counter/Counter.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/redux';
-import { decrement, increment } from '@/features/counterSlice';
+import { addNumber, decrement, increment, reset } from '@/features/counterSlice';
 
 export const Counter = () => {
+   const [number, setNumber] = useState(0);
 
-   const count = useAppSelector(state => state.counter.count);
+   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setNumber(Number(e.target.value));
+   };
+
+   const count = useAppSelector((state) => state.counter.count);
    const dispatch = useAppDispatch();
 
    return (
       <div>
          <p className={`font-semibold text-xl`}>{count}</p>
-         <button onClick={() => dispatch(increment())} className={'p-5'}>+</button>
-         <button onClick={() => dispatch(decrement())} className={'p-5'}>-</button>
+         <button onClick={() => dispatch(increment())} className={'p-5'}>
+            +
+         </button>
+         <button onClick={() => dispatch(decrement())} className={'p-5'}>
+            -
+         </button>
+         <button onClick={() => dispatch(reset())} className={'p-5'}>
+            Reset
+         </button>
+         <input
+            type={'number'}
+            value={number}
+            onChange={handleOnChange}
+         />
+         <button
+            onClick={() => dispatch(addNumber(number))}
+            className={'p-5'}
+         >
+            AddNumber
+         </button>
       </div>
    );
 };
