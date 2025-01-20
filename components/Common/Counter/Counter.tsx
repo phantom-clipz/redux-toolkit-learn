76be1b55629ba315d2010bf6eb437ed2ff63c072
last-chaanz <it21831768@my.sliit.lk>
2025-01-20 21:44:54 +0530

    file update

--- components/Common/Counter/Counter.tsx
+++ components/Common/Counter/Counter.tsx
@@ -5,38 +5,38 @@ import { useAppDispatch, useAppSelector } from '@/redux';
 import { addNumber, decrement, increment, reset } from '@/features/counterSlice';
 
 export const Counter = () => {
-   const [number, setNumber] = useState(0);
+    const [number, setNumber] = useState(0);
 
-   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setNumber(Number(e.target.value));
-   };
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNumber(Number(e.target.value));
+    };
 
-   const count = useAppSelector((state) => state.counter.count);
-   const dispatch = useAppDispatch();
+    const count = useAppSelector((state) => state.counter.count);
+    const dispatch = useAppDispatch();
 
-   return (
-      <div>
-         <p className={`font-semibold text-xl`}>{count}</p>
-         <button onClick={() => dispatch(increment())} className={'p-5'}>
-            +
-         </button>
-         <button onClick={() => dispatch(decrement())} className={'p-5'}>
-            -
-         </button>
-         <button onClick={() => dispatch(reset())} className={'p-5'}>
-            Reset
-         </button>
-         <input
-            type={'number'}
-            value={number}
-            onChange={handleOnChange}
-         />
-         <button
-            onClick={() => dispatch(addNumber(number))}
-            className={'p-5'}
-         >
-            AddNumber
-         </button>
-      </div>
-   );
+    return (
+        <div>
+            <p className={`font-semibold text-xl`}>{count}</p>
+            <button onClick={() => dispatch(increment())} className={'p-5'}>
+                +
+            </button>
+            <button onClick={() => dispatch(decrement())} className={'p-5'}>
+                -
+            </button>
+            <button onClick={() => dispatch(reset())} className={'p-5'}>
+                Reset
+            </button>
+            <input
+                type={'number'}
+                value={number}
+                onChange={handleOnChange}
+            />
+            <button
+                onClick={() => dispatch(addNumber(number))}
+                className={'p-5'}
+            >
+                AddNumber
+            </button>
+        </div>
+    );
 };
